39ebe1c0b56333a773da6d8966e14b1cabe3b824
WelleWest <info@wellewest.at>
2026-05-06 23:02:40 +0200

    Make sidebar icon visible: blue wave + WelleWest text

--- apps/cms-server/src/components/graphics/Icon.tsx
+++ apps/cms-server/src/components/graphics/Icon.tsx
@@ -2,14 +2,36 @@ import React from 'react'
 
 export const Icon: React.FC = () => {
   return (
-    <img
-      src="/wellewest-symbol.svg"
-      alt="WelleWest"
+    <div
       style={{
-        width: '32px',
-        height: '32px',
+        display: 'flex',
+        alignItems: 'center',
+        gap: '10px',
+        padding: '6px 0',
       }}
-    />
+    >
+      <img
+        src="/wellewest-symbol.svg"
+        alt=""
+        aria-hidden="true"
+        style={{
+          width: '60px',
+          height: 'auto',
+          display: 'block',
+        }}
+      />
+      <span
+        style={{
+          fontFamily: '"Amaranth", sans-serif',
+          fontWeight: 700,
+          fontSize: '15px',
+          color: 'var(--theme-text)',
+          letterSpacing: '-0.01em',
+        }}
+      >
+        WelleWest
+      </span>
+    </div>
   )
 }
 
